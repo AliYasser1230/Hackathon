f7386918509d0a13cb349952e3586f1849188a20
karim <kareem.mohamed18@msa.edu.eg>
2023-06-18 23:14:00 +0300

    commit2

--- hackson/Frontend/src/App.jsx
+++ hackson/Frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
+import Page from './pages/Page';
 import {Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 
 function App() {
   
   return (
+    <div>
     <Router>    
         <Routes>
           <Route exact path='/' element={<Login/>}/>
-          <Route exact path='/admin' element={<Admin/>}/>
+          <Route path='/admin' element={<Admin/>}/>
+          <Route path='/student' element={<Page/>}/>
         </Routes>
       
     </Router>
+    </div>
 
    
   );
--- hackson/Frontend/src/pages/Login.jsx
+++ hackson/Frontend/src/pages/Login.jsx
@@ -1,29 +1,91 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import logincss from '../css/Login.css'
+import axios from 'axios';
 
 const Login = () => {
+    const [logType, setLogType] = useState("student");
+    const [data,setData] = useState([]);
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const getUsers = async () => {
+        try {
+          const res = await axios.get('http://localhost:5050/getdata');
+          setData(res.data);
+        } catch (error) {
+          console.log(error.response.body);
+        }
+      };
+      useEffect(()=>{
+        getUsers();
+        console.log(data);
+      },[])
+    const handleLogin = (e)=>{
+        let flag = false;
+        e.preventDefault();
+        data.map((info)=>{
+            if(email == info.StudentID && password==info.Password)
+            {
+                flag = true;
+            }
+            console.log(info);
+        })
+        if(flag == true)
+        {
+            window.location.replace('http://localhost:3000/student');
+        }
+        else{
+            setError("Incorrect Information")
+        }
+    }
   return (
     <div>
-        <div class="container">
-        <h2>Login</h2>
-        <form action="/login" method="POST">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required/>
+        {
+            logType=="student"&&
+            <div class="container">
+            <h2>Student Login</h2>
+            <form action="/login" method="POST">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input onChange={(e)=>{setEmail(e.target.value)}} type="email" id="email" name="email" required/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input onChange={(e)=>{setPassword(e.target.value)}} type="password" id="password" name="password" required/>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Login" onClick={handleLogin}/>
+                </div>
+            </form>
+            <p>{error}</p>
+            <div class="admin-login-link">
+                <a onClick={()=>{setLogType('admin')}}>Login as Admin</a>
             </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required/>
-            </div>
-            <div class="form-group">
-                <input type="submit" value="Login as Student"/>
-            </div>
-        </form>
-
-        <div class="admin-login-link">
-            <a href="admin.html">Login as Admin</a>
         </div>
-    </div>
+        }
+        {
+            logType=="admin"&&
+            <div class="container">
+            <h2>Admin Login</h2>
+            <form action="/login" method="POST">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required/>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Login"/>
+                </div>
+            </form>
+            <div class="admin-login-link">
+                <a onClick={()=>{setLogType('student')}}>Login as Student</a>
+            </div>
+        </div>
+        }
+        
     </div>
   )
 }
